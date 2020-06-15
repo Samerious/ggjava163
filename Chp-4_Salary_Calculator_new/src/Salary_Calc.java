32eb8423ac45073c0e35c30104e50ed88125ac4f
Samerious <samerious@gmail.com>
2020-06-14 21:29:50 -0700

    Working overtime hours next for loop.

--- Chp-4_Salary_Calculator_new/src/Salary_Calc.java
+++ Chp-4_Salary_Calculator_new/src/Salary_Calc.java
@@ -4,16 +4,25 @@ public class Salary_Calc {
     static double wage;
     static double hoursworked;
     static double salary;
+    static String fullname;
+    static double overtimeworked;
 
     public static void main (String[] args) {
        Scanner input = new Scanner(System.in);
 
+       System.out.print("Enter the Employees Name: ");
+       fullname = input.nextLine();
        System.out.print("Enter the hours worked: ");
-       wage = input.nextInt();
-       System.out.print("Enter the hourly wage: ");
        hoursworked = input.nextInt();
-
-       double salary = wage * hoursworked;
-       System.out.printf ("Salary is $%,.2f%n",salary);
+       System.out.print("Enter the hourly wage: ");
+       wage = input.nextInt();
+       if (hoursworked <= 40.00) {
+           salary = wage * hoursworked;
+       }
+       else {
+           overtimeworked = hoursworked - 40;
+           salary = (wage * hoursworked) + (wage * overtimeworked * 1.5);
+       }
+       System.out.printf ("%s's salary is $%,.2f, and he worked %,.2f hours. %n ",fullname,salary,hoursworked);
     }
 }
